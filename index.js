0b89577069ff53706fa5d323dcf7b5a04b3a8fad
Kurt <knajimenez@gmail.com>
2023-02-22 19:43:15 +0800

    3.14

--- index.js
+++ index.js
@@ -50,47 +50,35 @@ app.get('/api/notes', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    console.log(id)
-    const note = notes.find(note => {
-        console.log(note.id, typeof note.id, id, typeof id, note.id === id)
-        return note.id === id
-    })
-
-    if (note) {
-        console.log(note)
+    Note.findById(request.params.id).then(note => {
         response.json(note)
-    } else {
-        response.status(404).send("Jumar was not found")
-    }
+    })
 })
 
-const generateId = () => {
+/* const generateId = () => {
     const maxId = notes.length > 0
         ? Math.max(...notes.map(n => n.id))
         : 0
     return maxId + 1
-}
+} */
 
 app.post('/api/notes', (request, response) => {
     const body = request.body
 
-    if (!body.content) {
+    if (body.content === undefined) {
         return response.status(400).json({
             error: 'Content missing'
         })
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
-        date: new Date(),
-        id: generateId(),
-    }
+    })
 
-    notes = notes.concat(note)
-
-    response.json(note)
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
