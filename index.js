834a916d020a55d679dc3b5482aa02dbe030b647
Kurt <knajimenez@gmail.com>
2023-02-23 19:00:29 +0800

    3.18

--- index.js
+++ index.js
@@ -16,12 +16,22 @@ const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
 
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
 app.use(express.static('build'))
 app.use(express.json())
 app.use(cors())
 app.use(requestLogger)
 
-let notes = [
+/* let notes = [
     {
         id: 1,
         content: "HTML is easy",
@@ -37,7 +47,7 @@ let notes = [
         content: "GET and POST are the most important methods of HTTP protocol",
         important: true
     }
-]
+] */
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
@@ -49,18 +59,18 @@ app.get('/api/notes', (request, response) => {
     })
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
     Note.findById(request.params.id).then(note => {
-        response.json(note)
+        if (note) {
+            response.json(note)
+        } else {
+            response.status(400).end()
+        }
     })
+        .catch(error => {
+            next(error)
+        })
 })
-
-/* const generateId = () => {
-    const maxId = notes.length > 0
-        ? Math.max(...notes.map(n => n.id))
-        : 0
-    return maxId + 1
-} */
 
 app.post('/api/notes', (request, response) => {
     const body = request.body
@@ -81,14 +91,35 @@ app.post('/api/notes', (request, response) => {
     })
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
 
-    response.status(204).end()
+    const note = {
+        content: body.content,
+        important: body.important,
+    }
+
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+        .then(updatedNote => {
+            response.json(updatedNote)
+        })
+        .catch(error => {
+            next(error)
+        })
+})
+
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => {
+            next(error)
+        })
 })
 
 app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
