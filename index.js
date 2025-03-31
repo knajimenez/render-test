bd22c8210c2afa0c893e19890f81a59b391dbd5c
Kurt <knajimenez@gmail.com>
2025-03-31 10:46:37 +0800

    New dependencies
    Added dotenv and mongoose

--- index.js
+++ index.js
@@ -1,24 +1,8 @@
+require('dotenv').config()
 const express = require('express')
-const cors = require('cors')
-const app = express()
+const Note = require('./models/note')
 
-let notes = [
-    {
-        id: '1',
-        content: 'HTML is easy',
-        important: true,
-    },
-    {
-        id: '2',
-        content: 'Browser can execute only JavaScript',
-        important: false,
-    },
-    {
-        id: '3',
-        content: 'GET and POST are the most important methods of HTTP protocol',
-        important: true,
-    },
-]
+const app = express()
 
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
@@ -29,7 +13,7 @@ const requestLogger = (request, response, next) => {
 }
 
 app.use(express.json())
-app.use(cors())
+app.use(express.static('dist'))
 app.use(requestLogger)
 
 app.get('/', (request, response) => {
@@ -37,7 +21,9 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then((notes) => {
+        response.json(notes)
+    })
 })
 
 app.get('/api/notes/:id', (request, response) => {
@@ -90,7 +76,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
