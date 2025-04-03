aff3e9ab20bcdc3613e18ded6abc5c0c6a8097bf
Kurt <knajimenez@gmail.com>
2025-04-03 16:11:59 +0800

    Updated index.js, and note.js
    Added mongoose validators

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
 
     next(error)
@@ -53,14 +55,8 @@ app.get('/api/notes/:id', (request, response, next) => {
         })
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
     const body = request.body
-
-    if (!body.content) {
-        return response.status(400).json({
-            error: 'content missing',
-        })
-    }
 
     const note = new Note({
         content: body.content,
@@ -70,6 +66,9 @@ app.post('/api/notes', (request, response) => {
     note.save()
         .then((savedNote) => {
             response.json(savedNote)
+        })
+        .catch((error) => {
+            next(error)
         })
 })
 
